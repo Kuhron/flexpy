a5d68f1af018422b9dade5e2959a2db021556f9c
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2020-11-12 06:47:14 +0000

    adding more explicit instructions to myself on how to release new versions

--- release_new_version.sh
+++ release_new_version.sh
@@ -3,3 +3,22 @@ rm -r build/
 rm -r dist/
 rm -r flexpy.egg-info/
 python setup.py sdist bdist_wheel
+
+
+# # upload to testpypi
+# twine upload --repository testpypi dist/*
+# # use virtualenv to test new version
+# mkvirtualenv flexpy
+# # (if it already exists, rmvirtualenv it and make new one)
+# workon flexpy
+# # (it will already put you in virtualenv after you make it, but workon is how you enter an existing one)
+# python -m pip install --index-url https://test.pypi.org/simple/ flexpy
+# # now new flexpy should be installed in the venv so you can try it out
+# python  # test stuff
+# deactivate  # exit virtualenv
+# rmvirtualenv flexpy
+
+
+# # upload to pypi
+# twine upload dist/*
+# python -m pip install flexpy --upgrade
